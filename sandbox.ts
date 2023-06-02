10b5fa53900005cc7641d3e4a0513f2ea82dbbf7
António Camarão <camaraoacsn@gmail.com>
2023-06-02 12:32:22 +0100

    #4 - Objects & Arrays

--- sandbox.ts
+++ sandbox.ts
@@ -1,20 +1,39 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+// Arrays
+let names = ['luigi', 'mario', 'yoshi'];
 
-//character = 20;
-character = 'luigi';
+// names = 'hello' -> can't change the array to a simple string
 
-//age = 'yoshi';
-age = 40;
+names.push('toad');
+// names.push(3); -> can't push a number to a string array
+// names[0] = 3; -> can't change a string to a number in a string array
 
-//isBlackBelt = 'yes';
-isBlackBelt = true;
+let numbers = [10, 20, 30, 40];
 
-//I can define the type i want that variable to be:
-//  variable: number, variable: string, variable: boolean
-const circ = (diameter: number) => {
-    return diameter * Math.PI
+numbers.push(25);
+// numbers.push('shaun'); -> can't push a string to a numbers array
+// numbers[1] = 'shaun'; -> can't change a number to a string in a numbers array
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3; // ken is replaced by 3
+
+// Objects
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30'; -> can't change a property with type number to type string
+// ninja.skills = ['fighting', 'sneaking']; -> can't add a new property that was not defined initially
+
+ninja = {
+    name : 'yoshi',
+    belt : 'orange',
+    age: 40
+    // age : 40 -> It needs to match the original structure of the object, can't change it completely without all the properties
 }
-
-console.log(circ(7.5));
