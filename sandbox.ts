c424c661f0cc9e948e3f9d7ea6a07fe15f756f26
António Camarão <camaraoacsn@gmail.com>
2023-06-02 12:17:16 +0100

    #3 - Type Basics

--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,20 @@
-const character = 'luigi';
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
 
-console.log(character);
+//character = 20;
+character = 'luigi';
 
-const inputs = document.querySelectorAll('input');
+//age = 'yoshi';
+age = 40;
 
-console.log(inputs);
+//isBlackBelt = 'yes';
+isBlackBelt = true;
 
-inputs.forEach(input => {
-    console.log(input);
-});
+//I can define the type i want that variable to be:
+//  variable: number, variable: string, variable: boolean
+const circ = (diameter: number) => {
+    return diameter * Math.PI
+}
+
+console.log(circ(7.5));
