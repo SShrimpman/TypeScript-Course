f85d790e18b828ad19d006da9e69acc399bd16cb
António Camarão <camaraoacsn@gmail.com>
2023-06-05 09:07:00 +0100

    #13 - Public, Private & Readonly

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 "use strict";
 // Classes
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
         return `${this.client} owes ${this.amount}€ for ${this.details}`;
@@ -15,9 +18,9 @@ const invTwo = new Invoice("Luigi", "work on the Luigi Website", 300);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invOne.client = "Yoshi";
-invTwo.amount = 400;
-console.log(invOne, invTwo);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 console.log(invoices);
 const form = document.querySelector('.new-item-form');
 // Inputs
